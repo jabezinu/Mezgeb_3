6d4b80becdc78bac5d83cd2d2c2b254ea558b836
jabezinu <jabezzinu31@gmail.com>
2025-07-20 10:51:09 +0300

    inital backend vercel fix

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import leadRoutes from './routes/leadRoutes.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -16,11 +16,26 @@ app.use(express.json());
 app.use('/api/clients', clientRoutes);
 app.use('/api/leads', leadRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-  console.log('MongoDB connected');
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-})
-.catch(err => {
-  console.error('MongoDB connection error:', err);
-});
+// mongoose.connect(process.env.MONGO_URI)
+// .then(() => {
+//   console.log('MongoDB connected');
+//   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// })
+// .catch(err => {
+//   console.error('MongoDB connection error:', err);
+// });
+
+
+// Vercel deployment
+let isConnected = false;
+
+async function connectDB() {
+    if (isConnected) return ;
+    await mongoose.connect(process.env.MONGO_URL);
+    isConnected = true;
+}
+
+export default async function handler(req, res) {
+    await connectDB();
+    app(req, res);
+}
