12ef36a5de5ddd2772af44dc19d6076818f71844
jabezinu <jabezzinu31@gmail.com>
2025-08-22 03:51:34 +0300

    Revert "inital backend vercel fix"
    This reverts commit 6d4b80becdc78bac5d83cd2d2c2b254ea558b836.

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import authRoutes from './routes/authRoutes.js';
 dotenv.config();
 
 const app = express();
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -21,26 +21,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/clients', clientRoutes);
 app.use('/api/leads', leadRoutes);
 
-// mongoose.connect(process.env.MONGO_URI)
-// .then(() => {
-//   console.log('MongoDB connected');
-//   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-// })
-// .catch(err => {
-//   console.error('MongoDB connection error:', err);
-// });
-
-
-// Vercel deployment
-let isConnected = false;
-
-async function connectDB() {
-    if (isConnected) return ;
-    await mongoose.connect(process.env.MONGO_URL);
-    isConnected = true;
-}
-
-export default async function handler(req, res) {
-    await connectDB();
-    app(req, res);
-}
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log('MongoDB connected');
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
+.catch(err => {
+  console.error('MongoDB connection error:', err);
+});
